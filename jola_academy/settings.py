a8eb8458899ce89ec31343ad3fabf81c4399e76c
Naveen Kadali <naveenkadali@outlook.com>
2025-06-15 01:56:26 +0530

    Restructured the project

--- jola_academy/settings.py
+++ jola_academy/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'jola_academy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'jola_academy' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
